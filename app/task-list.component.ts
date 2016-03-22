1e7fff926a096f9fb89cbcebc08466d4075b1b22
Jason Awbrey and lawton browning <student@epicodus.com>
2016-03-22 13:48:46 -0700

    Added a new task component

--- app/task-list.component.ts
+++ app/task-list.component.ts
@@ -1,13 +1,24 @@
 import { Component, EventEmitter } from 'angular2/core';
 import { TaskComponent } from './task.component';
 import { Task } from './task.model';
+import { EditTaskDetailsComponent } from './edit-task-details.component';
+import { NewTaskComponent } from './new-task.component';
 
 @Component({
   selector: 'task-list',
   inputs: ['taskList'],
   outputs: ['onTaskSelect'],
-  directives: [TaskComponent],
-  templateUrl: 'app/task-list.component.html'
+  directives: [TaskComponent, EditTaskDetailsComponent, NewTaskComponent],
+  template: `
+  <task-display *ngFor="#currentTask of taskList"
+    (click)="taskClicked(currentTask)"
+    [class.selected]="currentTask === selectedTask"
+    [task]="currentTask">
+  </task-display>
+  <edit-task-details *ngIf="selectedTask" [task]="selectedTask">
+  </edit-task-details>
+  <new-task (onSubmitNewTask)="createTask($event)"></new-task>
+  `
 })
 export class TaskListComponent {
   public taskList: Task[];
@@ -20,5 +31,10 @@ export class TaskListComponent {
     console.log('child', clickedTask);
     this.selectedTask = clickedTask;
     this.onTaskSelect.emit(clickedTask);
+  }
+  createTask(description: string): void {
+    this.taskList.push(
+      new Task(description, this.taskList.length)
+    )
   }
 }
