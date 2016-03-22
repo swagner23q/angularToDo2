dc828499038488612b6c17677452953c2f3b932d
Jason Awbrey and lawton browning <student@epicodus.com>
2016-03-22 16:43:04 -0700

    Add filter for priority

--- app/task-list.component.ts
+++ app/task-list.component.ts
@@ -3,21 +3,27 @@ import { TaskComponent } from './task.component';
 import { Task } from './task.model';
 import { EditTaskDetailsComponent } from './edit-task-details.component';
 import { NewTaskComponent } from './new-task.component';
-import {DonePipe} from './done.pipe';
+import { DonePipe } from './done.pipe';
+import { PriorityPipe } from './priority.pipe';
 
 @Component({
   selector: 'task-list',
   inputs: ['taskList'],
   outputs: ['onTaskSelect'],
-  pipes: [DonePipe],
+  pipes: [DonePipe, PriorityPipe],
   directives: [TaskComponent, EditTaskDetailsComponent, NewTaskComponent],
   template: `
-  <select (change)="onChange($event.target.value)" class="filter">
-    <option value="all" selected>Show All</option>
+  <select (change)="onChangeDone($event.target.value)" class="filter">
+    <option value="all">Show All</option>
     <option value="done">Show Done</option>
-    <option value="notDone">Show Not Done</option>
+    <option value="notDone" selected>Show Not Done</option>
   </select>
-  <task-display *ngFor="#currentTask of taskList | done:filterDone"
+  <select (change)="onChangePriority($event.target.value)" class="filter">
+    <option value="low" selected>low</option>
+    <option value="medium">medium</option>
+    <option value="high">high</option>
+  </select>
+  <task-display *ngFor="#currentTask of taskList | done:filterDone | priority:filterPriority"
     (click)="taskClicked(currentTask)"
     [class.selected]="currentTask === selectedTask"
     [task]="currentTask">
@@ -32,6 +38,7 @@ export class TaskListComponent {
   public onTaskSelect: EventEmitter<Task>;
   public selectedTask: Task;
   public filterDone: string = "notDone";
+  public filterPriority: string = "high";
   constructor() {
     this.onTaskSelect = new EventEmitter();
   }
@@ -43,7 +50,10 @@ export class TaskListComponent {
     newTask.id = this.taskList.length;
     this.taskList.push(newTask);
   }
-  onChange(filterOption) {
+  onChangeDone(filterOption) {
     this.filterDone = filterOption;
+  }
+  onChangePriority(filterOption) {
+    this.filterPriority = filterOption;
   }
 }
