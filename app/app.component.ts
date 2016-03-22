79f10c2f641a41569bf667d7f8762aab8caf5949
Jason Awbrey and lawton browning <student@epicodus.com>
2016-03-22 10:56:48 -0700

    Add custom event emmiter

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,11 +1,34 @@
-import { Component } from 'angular2/core';
+import { Component, EventEmitter } from 'angular2/core';
+
+@Component({
+  selector: 'task-list',
+  inputs: ['taskList'],
+  outputs: ['onTaskSelect'],
+  template: `
+  <h3 *ngFor="#currentTask of taskList" (click)="taskClicked(currentTask)">
+    {{ currentTask.description }}
+  </h3>
+  `
+})
+export class TaskListComponent {
+  public taskList: Task[];
+  public onTaskSelect: EventEmitter<Task>;
+  constructor() {
+    this.onTaskSelect = new EventEmitter();
+  }
+  taskClicked(clickedTask: Task): void {
+    console.log('child', clickedTask);
+    this.onTaskSelect.emit(clickedTask);
+  }
+}
 
 @Component({
   selector: 'my-app',
+  directives: [TaskListComponent],
   template: `
     <div class="container">
       <h1>To-Do List</h1>
-      <h3 *ngFor="#task of tasks" (click)="taskWasSelected(task)">{{ task.description }}</h3>
+      <task-list [taskList]="tasks" (onTaskSelect)="taskWasSelected($event)"></task-list>
     <div>
   `
 })
@@ -20,7 +43,7 @@ export class AppComponent {
   ];
   }
   taskWasSelected(clickedTask: Task): void {
-    console.log(clickedTask);
+    console.log('parent', clickedTask);
   }
 }
 
